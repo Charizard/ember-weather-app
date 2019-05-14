39a6c619c9b2ba9048c8d2c049ec0121af32ec42
Yuvaraja Balamurugan <yuv.slm@gmail.com>
2019-05-14 07:13:34 +0530

    Add loading state and search input for entering location names

--- app/pod/application/route.js
+++ app/pod/application/route.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+import { set, get } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
@@ -9,5 +10,13 @@ export default class ApplicationRoute extends Route {
     return this.geoLocation.fetchCurrentCoordinates().then((coords) => {
       return this.openWeather.fetchCurrentWeather(coords.latitude, coords.longitude);
     });
+  }
+
+  setupController(controller, model) {
+    // Call _super for default behavior
+    this._super(controller, model);
+
+    set(controller, 'model', model);
+    set(controller, 'location', get(model, 'name'));
   }
 }
--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('loading');
+  this.route('application-loading');
 });
 
 export default Router;
